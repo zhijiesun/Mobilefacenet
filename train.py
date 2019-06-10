0b5bde2996950c93e8ec4ab5ea448a69983fdb19
孙志杰 <48580814+JaydenGG@users.noreply.github.com>
2019-06-10 11:34:25 +0800

    Update train.py

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ train_datagen = ImageDataGenerator(rescale=1./255) #rescale the tensor values to
 train_image = train_datagen.flow_from_directory(
          base_dir,
          target_size=(112, 96),# I use the photo size (112*96)
-         batch_size=50,
+         batch_size=256,
          class_mode='categorical')
 
 
@@ -24,11 +24,13 @@ model_test.compile(loss='categorical_crossentropy',
             optimizer=optimizers.SGD(lr=0.01),
             metrics=['acc'])
 
-batchon=0
-for batch in train_image:
-    batch=train_image[0]
-    X_batch,label_batch=batch[:]
-    loss=model_test.train_on_batch([X_batch,label_batch],label_batch)
-    print("batchon batch:"+str(loss))
+
+epochs=10
+for e in range(epochs):
+    print('Epoch', e)
+    for batches in train_image:
+        X_batch, label_batch = batches[:]
+        print(len(X_batch))
+        model_test.fit([X_batch, label_batch],label_batch, epochs=3, batch_size=64)
 
 
